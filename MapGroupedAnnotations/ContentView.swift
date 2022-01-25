54cfb42cecad448c0458aeb1fa4dc96b1b44ece4
Snow Lukin <everyoursden@gmail.com>
2022-01-26 00:15:10 +0300

    Initial Commit

---
+++ MapGroupedAnnotations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapGroupedAnnotations
+//
+//  Created by Snow Lukin on 26.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapGroupedAnnotations/MapGroupedAnnotationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapGroupedAnnotationsApp.swift
+//  MapGroupedAnnotations
+//
+//  Created by Snow Lukin on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapGroupedAnnotationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
